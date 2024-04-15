812d587b307163660c5124216e2fee49049ed35f
lei.chen <lei.chen@amlogic.com>
2024-04-15 15:54:27 +0800

    8822cs: open rtw_odm_proc_open and rtw_adapter_proc_open [1/1]
    PD#OTT-58505
    
    Problem:
    1.The customer needs to implement these two interfaces
    2.Compilation passed
    
    Solution:
    fix it
    
    Verify:
    local build
    
    Change-Id: I185a845db61d5e4c3a08f6046c8faa2c932dcbb1
    Signed-off-by: lei.chen <lei.chen@amlogic.com>

--- rtl88x2CS/os_dep/linux/rtw_proc.c
+++ rtl88x2CS/os_dep/linux/rtw_proc.c
@@ -5853,9 +5853,6 @@ const int adapter_proc_hdls_num = sizeof(adapter_proc_hdls) / sizeof(struct rtw_
 
 static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
-	return -EROFS;
-#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
 	const struct rtw_proc_hdl *hdl = adapter_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
@@ -5878,7 +5875,6 @@ static int rtw_adapter_proc_open(struct inode *inode, struct file *file)
 	} else {
 		return -EROFS;
 	}
-#endif
 }
 
 static ssize_t rtw_adapter_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
@@ -6046,9 +6042,6 @@ const int odm_proc_hdls_num = sizeof(odm_proc_hdls) / sizeof(struct rtw_proc_hdl
 
 static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
-	return -EROFS;
-#else
 	ssize_t index = (ssize_t)PDE_DATA(inode);
 	const struct rtw_proc_hdl *hdl = odm_proc_hdls + index;
 	void *private = proc_get_parent_data(inode);
@@ -6071,7 +6064,6 @@ static int rtw_odm_proc_open(struct inode *inode, struct file *file)
 	} else {
 		return -EROFS;
 	}
-#endif
 }
 
 static ssize_t rtw_odm_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
